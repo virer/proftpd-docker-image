7c547c92c784f2a16c5b453cc16c38514574e87e
scaps-nrb <sebastien.caps@nrb.be>
2021-03-12 17:14:36 +0100

    Fix if/fi

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
-if test -e /etc/sftp-config/ftp.passwd; then cat /etc/sftp-config/ftp.passwd > /usr/local/etc/ftp/ftp.passwd 
-if test -e /etc/sftp-config/ftp.group;  then cat /etc/sftp-config/ftp.group > /usr/local/etc/ftp/ftp.group 
+if test -e /etc/sftp-config/ftp.passwd; then 
+    cat /etc/sftp-config/ftp.passwd > /usr/local/etc/ftp/ftp.passwd 
+fi
+if test -e /etc/sftp-config/ftp.group;  then 
+    cat /etc/sftp-config/ftp.group > /usr/local/etc/ftp/ftp.group 
+fi
 
 /usr/local/sbin/proftpd -n -c /usr/local/etc/proftpd/proftpd.conf
 
